a44ae3b5703dd1625d604c4393d6ca64831b81c0
gisellyreis <gisellyareis@gmail.com>
2022-12-19 22:30:22 -0300

    start reactjs - fundamentus CSS Module

---
+++ fundamentos/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/Header";
+import { Post } from "./Post";
+
+ 
+export function App() {
+
+  return (
+    <div>
+      <Header />
+      <Post author="" content="" />
+    </div>
+  )
+}
---
+++ fundamentos/src/Post.jsx
@@ -0,0 +1,10 @@
+import "./styles.css"
+
+export function Post(props) {
+  return (
+    <div>
+      <strong> {props.author} </strong>
+      <p> {props.content} </p>
+    </div>
+  )
+}
