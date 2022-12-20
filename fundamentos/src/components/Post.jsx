e1863567c3806a7cc9b104c068d45a56d40584b7
gisellyreis <gisellyareis@gmail.com>
2022-12-20 11:42:46 -0300

    Estrutura da aplicação - Componente: Post

---
+++ fundamentos/src/components/Post.jsx
@@ -0,0 +1,40 @@
+import styles from './Post.module.css'
+
+export function Post(props) {
+  return (
+    <div>
+      <article className={styles.post}>
+        <header>
+          <div className={styles.author}>
+            <img className={styles.avatar} src="https://github.com/gisellyreis.png" />
+            <div className={styles.authorInfo}>
+              <strong>Giselly Reis</strong>
+              <span>Web Developer</span>
+            </div>
+          </div>
+
+          <time 
+            title="20 de Dezembro às 11:15h" 
+            dateTime="2022-12-20 11:15:00"
+          >
+            Publicado há 1h
+          </time>
+        </header>
+
+        <div className={styles.content}>
+          <p>Fala galeraa 👋</p>
+
+          <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+          <p>👉 <a href="" > jane.design/doctorcare</a></p>
+
+          <p>
+            <a href="" >#novoprojeto </a>
+            <a href="" >#nlw </a>
+            <a href="" >#rocketseat</a>
+          </p>
+        </div>
+      </article>
+    </div>
+  )
+}
