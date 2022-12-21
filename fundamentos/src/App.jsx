a4fa26bb551b216790f5065947656febfaf612cf
gisellyreis <gisellyareis@gmail.com>
2022-12-21 19:13:10 -0300

    Os motores do React Listas; Propriedades; Estado (useState); Key; Imutabilidade.

--- fundamentos/src/App.jsx
+++ fundamentos/src/App.jsx
@@ -4,6 +4,45 @@ import { Post } from "./components/Post";
 
 import './global.css'
 import styles from './App.module.css'
+
+// author: {avatar_url: "", name: "", role: ""}
+// publishedAt: Date
+// content: string 
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gisellyreis.png',
+      name: 'Giselly Reis',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋',},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+
+      { type: 'link', content: '👉 jane.design/doctorcare',}
+
+    ],
+    publishedAt: new Date('2022-12-10')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/samuelRios.png',
+      name: 'Samuel Rios',
+      role: 'Web Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋',},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀',},
+
+      { type: 'link', content: '👉 jane.design/doctorcare',}
+
+    ],
+    publishedAt: new Date('2022-12-20')
+  },
+]
  
 export function App() {
 
@@ -14,7 +53,16 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post author="" content="" />
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author} 
+                content={post.content} 
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
--- fundamentos/src/components/Comment.jsx
+++ fundamentos/src/components/Comment.jsx
@@ -1,7 +1,13 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
-export function Comment() {
+
+export function Comment({ content, onDeleteComment }) {
+
+  function handleDeleteComment() {
+    onDeleteComment(content)
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="https://github.com/gisellyreis.png" />
@@ -19,11 +25,11 @@ export function Comment() {
               </time>
             </div>
 
-            <button title="Deletar comentário">
+            <button onClick={handleDeleteComment} title="Deletar comentário">
               <Trash size={24} />
             </button>
           </header>
-          <p>Muito bom Devon, parabéns.</p>
+          <p> { content } </p>
         </div>
 
         <footer>
