31089b612879e37364282fe5cbb6bc328b769a80
gisellyreis <gisellayreis@gmail.com>
2023-01-05 12:23:20 -0300

    Estrutura da Home

--- ignite-shop/next.config.js
+++ ignite-shop/next.config.js
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    swcMinify: true,
+
+    experimental: {
+        images: {
+            allowFutureImage: true,
+        },
+    },
 }
 
 module.exports = nextConfig
