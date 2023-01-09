1156f59c2ac776bbaff0aae301264f5538f02fe4
gisellyreis <gisellyareis@gmail.com>
2023-01-09 13:12:28 -0300

    Estrutura da página de sucesso

--- ignite-shop/.next/static/chunks/webpack.js
+++ ignite-shop/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5cfa1d8ffb304f43"; }
+/******/ 		__webpack_require__.h = function() { return "8032683155b7b85f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
